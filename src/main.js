20ae256f6884da939ee6614b7260050aed184286
Miquéias Fernandes <contatomiqueiasfernandes@hotmail.com>
2022-10-25 20:14:47 -0300

    adicionei G analytics

--- src/main.js
+++ src/main.js
@@ -22,3 +22,25 @@ router.beforeEach((to) => {
 app.use(router)
 app.use(head)
 app.mount(document.body)
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNOpScJm3iP9ErR6sQeXim7OFZ_Vg4HwU",
+  authDomain: "geneapp-55cf9.firebaseapp.com",
+  projectId: "geneapp-55cf9",
+  storageBucket: "geneapp-55cf9.appspot.com",
+  messagingSenderId: "738472872889",
+  appId: "1:738472872889:web:1dffc2f9049476b02d8468",
+  measurementId: "G-5T3VLTX3BE"
+};
+
+// Initialize Firebase
+const app_firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app_firebase);
